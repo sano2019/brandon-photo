a63993f30c1dcc8410b06c5d8c29693fc5ca3bc9
Sander Nobel <sander.nobel@protonmail.com>
2021-08-30 16:41:17 +0200

    Style fix

--- pages/_document.js
+++ pages/_document.js
@@ -16,8 +16,6 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet" />
           <link rel="icon" href="/favicon.png" type="image/png" />
         </Head>
-        <style jsx global>{ reset }</style>
-        <style jsx global>{ globals }</style>
         <body>
           <Main />
           <NextScript />
